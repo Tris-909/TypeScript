a4f2ae95c424dc8a9725b45a30e584ee82b3372b
= <tranminhtri9090@gmail.com>
2021-01-04 13:40:57 +1000

    Implementating Sort for Strings

--- sort/build/index.js
+++ sort/build/index.js
@@ -2,7 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
 var NumberCollections_1 = require("./NumberCollections");
+var CharactersCollection_1 = require("./CharactersCollection");
+//TODO: Sort numbers
 var numberCollection = new NumberCollections_1.NumberCollection([10, -1, -3, -5, 5, 2]);
 var sorter = new Sorter_1.Sorter(numberCollection);
 sorter.sort();
-console.log(sorter.collection.data);
+console.log(numberCollection.data);
+//TODO: Sort Strings
+var charsCollection = new CharactersCollection_1.CharactersCollection('DcBa');
+var secSorter = new Sorter_1.Sorter(charsCollection);
+secSorter.sort();
+console.log(charsCollection.data);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,15 @@
 import { Sorter } from './Sorter';
 import { NumberCollection } from './NumberCollections';
+import { CharactersCollection } from './CharactersCollection';
 
+//TODO: Sort numbers
 const numberCollection = new NumberCollection([10, -1, -3, -5, 5, 2]);
 const sorter = new Sorter(numberCollection);
 sorter.sort();
-console.log(sorter.collection.data);
+console.log(numberCollection.data);
+
+//TODO: Sort Strings
+const charsCollection = new CharactersCollection('DcBa');
+const secSorter = new Sorter(charsCollection);
+secSorter.sort();
+console.log(charsCollection.data);
